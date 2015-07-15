9427b80b7c50610ee590e4725c42f5721cda0b5f
Jonathan Gyurkovics <jdgyurkovics@fullsail.edu>
2015-07-14 22:36:29 -0400

    Made the base project.

---
+++ Debugger/Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	[SerializeField]
+	float Speed;
+
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<Renderer>().material.color = Color.red;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate (){
+		Vector3 pos = transform.localPosition;
+
+		pos.x = pos.x + Input.GetAxisRaw ("Horizontal") * Speed * Time.deltaTime;
+		pos.z = pos.z + Input.GetAxisRaw ("Vertical") * Speed * Time.deltaTime;
+
+		transform.localPosition = pos;
+	}
+}
