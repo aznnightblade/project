985b922791c66b5ff699048d1b051d6cb637f6de
Jonathan Gyurkovics <jdgyurkovics@fullsail.edu>
2015-07-16 22:34:42 -0400

    Trying to fix shit.

---
+++ Debugger/Assets/Scripts/GoToTeleporter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToTeleporter : MonoBehaviour {
+
+	[SerializeField]
+	Transform teleporter = null;
+
+	[SerializeField]
+	Transform teleportLocation = null;
+
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<Renderer>().material.color = Color.green;
+	}
+	
+	void OnDrawGizmos (){
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireCube(teleportLocation.position, teleporter.localScale);
+	}
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.gameObject.tag == "Player") {
+			Vector3 teleportTo = teleportLocation.transform.position;
+			teleportTo.y = col.gameObject.transform.localPosition.y;
+
+			col.gameObject.transform.localPosition = teleportTo;
+		}
+	}
+}
