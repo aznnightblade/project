bc24b188fa9f22825baa897c430fd68b329d551f
Jonathan Gyurkovics <jdgyurkovics@fullsail.edu>
2015-07-20 17:26:27 -0400

    Added Corruption for blocking paths. Updated the player's sprite to be at the appropriate location.

---
+++ Debugger/Assets/Scripts/CorruptionScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CorruptionScript : MonoBehaviour {
+
+	[SerializeField]
+	int health = 100;
+	[SerializeField]
+	int defense = 2;
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == "Player Bullet") {
+			float bulletScale = col.gameObject.GetComponentInChildren<Transform>().localScale.x / 0.2f;
+			PlayerStatistics bulletOwner = col.gameObject.GetComponent<BulletScript>().Owner;
+
+			health -= Mathf.CeilToInt(bulletOwner.Damage + bulletOwner.Damage *(bulletOwner.ChargedDamageScale * (bulletScale - 1)));
+
+			Destroy(col.gameObject);
+
+			if(health <= 0)
+				Destroy(gameObject);
+		}
+	}
+}
