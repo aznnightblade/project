4b1aa4d8205c07016623f2350e38abd9a8178091
Michael Lin <AznNightblade@gmail.com>
2015-07-20 18:47:22 -0400

    added dialog

---
+++ Debugger/Assets/Dialog.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dialog : MonoBehaviour {
+
+    public string[] Questions;
+    bool DisplayDialog = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(700, 600,400,400));
+            if(DisplayDialog==true)
+            {
+                GUILayout.Label(Questions[0]);
+                GUILayout.Label(Questions[1]);
+                GUILayout.Label(Questions[2]);
+            }
+
+            GUILayout.EndArea();
+    }
+    void OnTriggerEnter()
+    {
+        DisplayDialog = true;
+    }
+    void OnTriggerExit()
+    {
+        DisplayDialog = false;
+    }
+}
