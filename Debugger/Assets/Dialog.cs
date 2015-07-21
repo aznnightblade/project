64beba18bd4a2cf824dfde561a433e5f04611d82
Michael Lin <AznNightblade@gmail.com>
2015-07-20 20:18:17 -0400

    HUD World interactions

---
+++ Debugger/Assets/Dialog.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dialog : MonoBehaviour {
+    public string[] Tutorial;
+    bool Dialoguebox = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(800, 600, 400, 400));
+        if (Dialoguebox == true)
+            {
+                GUILayout.Label(Tutorial[0]);
+                GUILayout.Label(Tutorial[1]);
+                GUILayout.Label(Tutorial[2]);
+            }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag=="Player")
+        {
+            Dialoguebox = true;
+        }
+       
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Dialoguebox = false;
+        }
+    }
+}
---
+++ Debugger/Assets/Levels.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Levels : MonoBehaviour {
+    public string[] Destination;
+
+    bool Dialoguebox = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(800, 600, 400, 400));
+        if (Dialoguebox == true)
+        {
+            GUILayout.Label(Destination[0]);
+            if (GUILayout.Button(Destination[1]))
+            {
+                Dialoguebox = false;
+                Application.LoadLevel("Tutorial");
+            }
+            if (GUILayout.Button(Destination[2]))
+            {
+                Dialoguebox = false;
+                Application.LoadLevel("TestEnvironment");
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Dialoguebox = true;
+        }
+
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Dialoguebox = false;
+        }
+    }
+}
