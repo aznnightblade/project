de3d0e2f6a47a956d602ba07e9b7d2fce94c29c3
Darryl Warner <dvwarner@fullsail.edu>
2015-07-20 20:02:51 -0400

    Enemies move now!  Needs improvement, though.

--- Debugger/Assets/Scripts/EnemyBehavior.cs
+++ Debugger/Assets/Scripts/EnemyBehavior.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class EnemyBehavior : MonoBehaviour {
@@ -6,12 +6,12 @@ public class EnemyBehavior : MonoBehaviour {
 	[SerializeField]
 	EnemyStatistics enemy;
 	[SerializeField]
-	float Speed = 5.0f;
+	float Speed = 2.5f;
 	[SerializeField]
 	bool Ranged = false;
 
-	int maxHealth = 0;
-	int currHealth = 0;
+	Transform target = null;
+
 
 
 	// Use this for initialization
@@ -19,12 +19,27 @@ public class EnemyBehavior : MonoBehaviour {
 		gameObject.GetComponent<Renderer> ().material.color = Color.magenta;
 		gameObject.GetComponent<Rigidbody> ().maxAngularVelocity = Speed;
 		enemy = gameObject.GetComponent<EnemyStatistics> ();
-		maxHealth = currHealth = 10 * enemy.Endurance;
+		target = GameObject.FindGameObjectWithTag ("Player").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (currHealth == 0)
+		if (enemy.Health == 0)
 			Destroy (gameObject);
+	}
+
+	void FixedUpdate() {
+		Vector3 pos = transform.position;
+
+		Vector3 targetPoint = target.position;
+		Vector3 distance = target.position - pos;
+		distance.Normalize ();
+
+		pos.x = pos.x + distance.x * Speed * Time.deltaTime;
+		pos.z = pos.z + distance.z * Speed * Time.deltaTime;
+
+		transform.position = pos;
+
+
 	}
 }
