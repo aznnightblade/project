79a539efe5f299952fe1cc9776cdd9d7c8fa2fb6
Brandon Rodriguez <bwrodriguez@fullsail.edu>
2015-07-21 17:04:16 -0400

    Tutorial Level Built

---
+++ Debugger/Assets/Scripts/GoToHub.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToHub : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void OnTriggerEnter(Collider col)
+	{
+		Application.LoadLevel("HudWorld");
+		
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Debugger/Assets/Tutorial.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tutorial : MonoBehaviour {
+	public string []Corruption;
+	bool Dialoguebox = false;
+	// Use this for initialization
+	void Start () {
+	
+	
+	}
+	void OnGUI()
+	{
+		GUILayout.BeginArea(new Rect(800, 600, 400, 400));
+		if (Dialoguebox == true)
+		{
+			GUILayout.Label(Corruption[0]);
+			GUILayout.Label(Corruption[1]);
+			GUILayout.Label(Corruption[2]);
+		}
+	}
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag=="Player")
+		{
+			Dialoguebox = true;
+		}
+		
+	}
+	void OnTriggerExit(Collider col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			Dialoguebox = false;
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
