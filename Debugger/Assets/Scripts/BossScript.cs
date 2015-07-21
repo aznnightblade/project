6c8d1af9706e57b070c571c28b1df0dab4f09c46
Jonathan Gyurkovics <jdgyurkovics@fullsail.edu>
2015-07-21 15:49:22 -0400

    Added boss waypoints and started the script. Added the waypoints to the map.

---
+++ Debugger/Assets/Scripts/BossScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossScript : MonoBehaviour {
+
+	[SerializeField]
+	EnemyStatistics  boss = null;
+	[SerializeField]
+	Transform bullet = null;
+	[SerializeField]
+	Transform target = null;
+	
+	float slowTimer = 0.0f;
+	[SerializeField]
+	float ShootDelay = 0.25f;
+	[SerializeField]
+	float ReductionPerAgility = 0.005f;
+	float fireTimer = 0.0f;
+
+	[SerializeField]
+	float activateDistance = 2.0f;
+
+	bool active = false;
+
+	Random rnd = new Random();
+
+	private NavMeshAgent agent = null;
+	[SerializeField]
+	Transform[] Waypoints;
+	Vector3 destination = Vector3.zero;
+
+	void Start() {
+		agent = gameObject.GetComponent<NavMeshAgent>();
+		destination = Waypoints[Random.Range(0,Waypoints.Length)].transform.position;
+	}
+
+	void Update() {
+		if (active == false) {
+			if(Vector3.Distance(target.position, transform.position) <= activateDistance){
+				active = true;
+			}
+		} else {
+			if(gameObject.GetComponent<NavMeshAgent>().remainingDistance <= 3) {
+				destination = Waypoints[Random.Range(0,Waypoints.Length)].transform.position;
+			}
+		}
+	}
+}
