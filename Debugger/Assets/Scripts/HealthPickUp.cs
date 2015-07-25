4972751da4c0bf4933cc0fe56fcdb6c5998421e7
Michael Lin <AznNightblade@gmail.com>
2015-07-25 13:41:01 -0400

    added more sound

--- Debugger/Assets/Scripts/HealthPickUp.cs
+++ Debugger/Assets/Scripts/HealthPickUp.cs
@@ -1,16 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.Audio;
 public class HealthPickUp : MonoBehaviour {
 
+    public Soundmanager sounds;
+   void Start()
+    {
+        sounds = GameObject.FindGameObjectWithTag("Sound Manager").GetComponent<Soundmanager>();
+    }
 	void OnTriggerEnter (Collider other) {
 		if (other.tag == "Player") {
 			PlayerStatistics player = other.gameObject.GetComponent<PlayerStatistics>();
-
+            sounds.Sounds[3].Play();
 			// If player is at least 20% below max health
-			if(player.Health <= player.MaxHealth - (player.MaxHealth / 10)) {
+			if(player.Health <= player.MaxHealth - (player.MaxHealth / 5)) {
 				// Restore 10% of max health to player
-				player.Health += player.MaxHealth / 10;
+				player.Health += player.MaxHealth / 5;
 			} else {
 				// Prevent overhealing
 				player.Health = player.MaxHealth;
